ecf92af5206b57fc0aee0e6a3b12946e4f3b2553
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-12-03 13:11:04 +0100

    i created a server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen("3000", () => {
+  console.log("the server started listening");
+});
