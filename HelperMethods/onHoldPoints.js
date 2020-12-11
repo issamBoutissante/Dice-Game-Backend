cd580fedaa26f8e1faadac596fc97a194e5ddb62
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-12-11 22:01:22 +0100

    still working

--- HelperMethods/onHoldPoints.js
+++ HelperMethods/onHoldPoints.js
@@ -1,6 +1,6 @@
 const onHoldPoints = (socket, io) => {
   socket.on("Hold", ({ roomId, score }) => {
-    console.log("dice Holded");
+    console.log("dice Holded and the score is " + score);
     io.to(roomId).emit("ScoreHolded", { score });
   });
 };
--- HelperMethods/onRollDice.js
+++ HelperMethods/onRollDice.js
@@ -2,7 +2,7 @@ const onRollDice = (socket, io) => {
   socket.on("RollDice", ({ roomId }) => {
     console.log(roomId);
     console.log("Dice Rolled");
-    let ranNum = Math.ceil(Math.random() * 6);
+    let ranNum = Math.floor(Math.random() * 6 + 1);
     console.log(ranNum);
     io.to(roomId).emit("DiceRolled", { ranNum });
   });
